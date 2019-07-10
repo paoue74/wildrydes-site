7126b05b6a6759106dcff36a33c2e5992901d8db
paoue74 <opaf.info@gmail.com>
2019-07-10 09:39:57 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_ZLD5b6xuM', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1fdhnsr38k97j4e5ljmhrjb6qq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
